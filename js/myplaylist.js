8b03257d9417a18b0087e881532bfa25a6c8b75e
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-07-20 16:44:51 +0100

    Initial Commit

---
+++ js/myplaylist.js
@@ -0,0 +1,14 @@
+var myPlaylist = [
+
+    {
+        mp3:'../audio/theboxPart1.mp3',
+        oga:'',
+        title:'The Box (part 1)',
+        artist:'Orbital',
+        rating:4.5,
+        buy:'#',
+        price:'0.99',
+        duration:'',
+        cover:'../images/stock/bridge.jpg'
+    }
+];
